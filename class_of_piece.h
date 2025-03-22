21bbd9d3c62eec76c3c89b2538387fc3756d15c6
Muhammed Ahmed <ma0950082@gmail.com>
2025-03-22 18:04:17 +0200

    i midified somethings in the pieces.h and created board.h

--- class_of_piece.h
+++ class_of_piece.h
@@ -1,18 +1,16 @@
 #include<set>
+#include"class_of_board.h"
+using namespace std;
 class piece
 {
 private:
-    struct coordinates
-    {
-        int x, y;
-    };
     int id;
-    piece::coordinates location;
+    coor location;
     bool color;
-    piece::coordinates square_occiped; 
+    coor square_occiped;
 public:
-    virtual bool bin() = 0;
-    virtual bool move(piece::coordinates input) = 0;
-    virtual std::set<piece::coordinates> range_of_motion() = 0;
+    virtual bool bin(coor input) = 0;
+    virtual bool move(coor input) = 0;
+    virtual set<coor> range_of_motion() = 0;
     virtual void die() = 0;
 };
